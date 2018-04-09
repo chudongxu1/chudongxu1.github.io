d32c673feaf2f0577fc763be3eed0f10ebcb708c
褚东旭 <2176213131@qq.com>
2018-04-09 22:02:10 +0800

    增加了

--- 个人博客/shoping/js/jqzoom.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-
-$(function(){
-	                                       //offset小图大图之间距离
-	$(".jqzoom").jqueryzoom({xzoom:300,yzoom:300,offset:50});
-})
--- 个人博客/华为/js/script2.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by lenovo on 18/3/23 023.
- */
-if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))){
-    (function(){
-        window.scrollReveal = new scrollReveal({reset: true});
-    })();
-};
